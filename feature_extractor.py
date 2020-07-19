eedc3607ffc5dfcca82d522f91a33bc7ba2afcbf
Alireza Rahimi <ali938414@gmail.com>
2020-07-19 13:25:12 +0430

    add header to csv add image name to first of each row

--- feature_extractor.py
+++ feature_extractor.py
@@ -6,13 +6,25 @@ from tensorflow.keras import layers
 
 IMG_HEIGHT = 224
 IMG_WIDTH = 224
-BATCH_SIZE = 32
+BATCH_SIZE = 5
 IMG_SHAPE = [IMG_WIDTH, IMG_HEIGHT, 3]
 
 # load data
-data_dir = [('/home/alireza/projects/python/outlier/outlier_detection/datasets/instagram', "instagram")]
+data_dir = [('/home/alireza/projects/python/outlier/outlier_detection/datasets/flickr', "flickr")]
 output_dir = './extracted_feature/'
 extractor_images_output_dir = './feature_extractor_images/'
+
+
+def beautify(all_array, train_data_gen):
+    column_to_be_added = np.array(train_data_gen.filenames)
+    get_name = lambda t: str(t).split("/")[-1]
+    column_to_be_added = np.array([get_name(xi) for xi in column_to_be_added])
+    result = np.hstack((np.atleast_2d(column_to_be_added).T, all_array))
+    header = "img"
+    for i in range(result.shape[1] - 1):
+        header = header + "," + str(i)
+    data_format = ["%s"] + ["%s" for i in range(result.shape[1] - 1)]
+    return result, header, data_format
 
 
 def sample_step(input_image, model):
@@ -38,8 +50,8 @@ def load_model_make_csv_50v2(train_data_gen, name):
         image_batch, _ = train_data_gen.next()
         all_array = np.concatenate((sample_step(image_batch, model), all_array), axis=0)
 
-    print(all_array.shape)
-    np.savetxt(output_dir + name + '_ResNet50V2.csv', all_array, delimiter=",")
+    all_array, header, data_format = beautify(all_array, train_data_gen)
+    np.savetxt(output_dir + name + '_ResNet50V2.csv', all_array, fmt=data_format, header=header, delimiter=",")
 
 
 def load_model_make_csv_101v2(train_data_gen, name):
@@ -60,8 +72,8 @@ def load_model_make_csv_101v2(train_data_gen, name):
         image_batch, _ = train_data_gen.next()
         all_array = np.concatenate((sample_step(image_batch, model), all_array), axis=0)
 
-    print(all_array.shape)
-    np.savetxt(output_dir + name + '_ResNet101V2.csv', all_array, delimiter=",")
+    all_array, header, data_format = beautify(all_array, train_data_gen)
+    np.savetxt(output_dir + name + '_ResNet101V2.csv', all_array, fmt=data_format, header=header, delimiter=",")
 
 
 def load_model_make_csv_152v2(train_data_gen, name):
@@ -82,8 +94,8 @@ def load_model_make_csv_152v2(train_data_gen, name):
         image_batch, _ = train_data_gen.next()
         all_array = np.concatenate((sample_step(image_batch, model), all_array), axis=0)
 
-    print(all_array.shape)
-    np.savetxt(output_dir + name + '_ResNet152V2.csv', all_array, delimiter=",")
+    all_array, header, data_format = beautify(all_array, train_data_gen)
+    np.savetxt(output_dir + name + '_ResNet152V2.csv', all_array, fmt=data_format, header=header, delimiter=",")
 
 
 def load_model_make_csv_121(train_data_gen, name):
@@ -104,8 +116,8 @@ def load_model_make_csv_121(train_data_gen, name):
         image_batch, _ = train_data_gen.next()
         all_array = np.concatenate((sample_step(image_batch, model), all_array), axis=0)
 
-    print(all_array.shape)
-    np.savetxt(output_dir + name + '_DenseNet121.csv', all_array, delimiter=",")
+    all_array, header, data_format = beautify(all_array, train_data_gen)
+    np.savetxt(output_dir + name + '_DenseNet121.csv', all_array, fmt=data_format, header=header, delimiter=",")
 
 
 def load_model_make_csv_169(train_data_gen, name):
@@ -126,8 +138,8 @@ def load_model_make_csv_169(train_data_gen, name):
         image_batch, _ = train_data_gen.next()
         all_array = np.concatenate((sample_step(image_batch, model), all_array), axis=0)
 
-    print(all_array.shape)
-    np.savetxt(output_dir + name + '_DenseNet169.csv', all_array, delimiter=",")
+    all_array, header, data_format = beautify(all_array, train_data_gen)
+    np.savetxt(output_dir + name + '_DenseNet169.csv', all_array, fmt=data_format, header=header, delimiter=",")
 
 
 def load_model_make_csv_201(train_data_gen, name):
@@ -148,8 +160,8 @@ def load_model_make_csv_201(train_data_gen, name):
         image_batch, _ = train_data_gen.next()
         all_array = np.concatenate((sample_step(image_batch, model), all_array), axis=0)
 
-    print(all_array.shape)
-    np.savetxt(output_dir + name + '_DenseNet201.csv', all_array, delimiter=",")
+    all_array, header, data_format = beautify(all_array, train_data_gen)
+    np.savetxt(output_dir + name + '_DenseNet201.csv', all_array, fmt=data_format, header=header, delimiter=",")
 
 
 def load_all_models(path, name):
